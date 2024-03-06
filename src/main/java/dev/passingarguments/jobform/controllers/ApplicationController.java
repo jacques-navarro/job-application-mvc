4409ac2f7eeae8be88b47a7c76f74fc7b258c7bc
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-03-06 16:34:23 +0100

    Add application-confirmation.html

--- src/main/java/dev/passingarguments/jobform/controllers/ApplicationController.java
+++ src/main/java/dev/passingarguments/jobform/controllers/ApplicationController.java
@@ -4,6 +4,8 @@ import dev.passingarguments.jobform.models.Application;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ApplicationController {
@@ -12,5 +14,10 @@ public class ApplicationController {
     public String showApplicationForm(Model model) {
         model.addAttribute("application", new Application());
         return "application-form";
+    }
+
+    @PostMapping("/process-application")
+    public String processApplication(@ModelAttribute("application") Application application) {
+        return "application-confirmation";
     }
 }
